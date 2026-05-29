a20f571a059c4f42161265ed153d02e299316c78
Iozhewa Doshie <yeshua.delapena@gmail.com>
2026-05-29 16:02:51 +1000

    Parser object sprint of Interpreter, hasn't passed informal testing

--- Round9-Runs1to15/Interpreter.py
+++ Round9-Runs1to15/Interpreter.py
@@ -2,15 +2,18 @@
 import time
 
 class Interpreter:
-    def __init__(self, filepath, click):
+    def __init__(self, filepath):
         self.filepath:str = filepath
-        self.click:int = click
+        self.click:int = 0
+        self.runtime:float = 0.0
+        self.header:list[str] = []
+        self.data:dict[str, list[float]] = []
     def __str__(self):
         f"Interpreter(path={self.filepath})"
     
-    def parse(self):
+    def __parse(self) -> None:
         try:
-            self.elapse()
+            self.__elapse()
             with open(self.filepath, 'r') as reader:
                 lines:list[str] = reader.readlines()
         except FileNotFoundError:
@@ -18,16 +21,36 @@ class Interpreter:
         except Exception as e:
             print(f"Interpreter: Unknown error '{e}'")
         else:
-            pass
-            #  self.elapse()
+            self.header = lines[0].split(';')
+            #  lines[1] has not been designated a variable yet
+            acronyms:list[str] = [entry for entry in lines[2].split()]
+            self.data = {a: [] for a in acronyms}
+
+            for line in lines[2:]:
+                for index, datapoint in enumerate(line.split()):
+                    key:str = acronyms[index]
+                    self.data[key].append(datapoint)
+            self.runtime = self.__elapse()
+            return
     
-    def elapse(self, initial=0, final=0) -> float:
+    def __elapse(self, initial=0, final=0) -> float:
         if click == 0:
             initial = time.time()
             click += 1
         else:
             final = time.time()
             return round(final-initial, 2)
+    
+    def summary(self) -> str:
+        self.__parse()
+        return f'''{'-'*25}
+Interpreter opened '{self.filepath}' with the following information:
+{' \n'.join(self.header)}
+Parsing completed in {self.runtime} seconds.
+{'-'*25}'''
 
 if __name__ == "__main__":
     print(".")
+    path = "B2356raw2.dat"
+    inter = Interpreter(path)
+    print(inter.summary())
