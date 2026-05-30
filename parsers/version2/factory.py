9b6c678b8b431ee959ccca65e195a267ae6ab9bf
Iozhewa Doshie <yeshua.delapena@gmail.com>
2026-05-30 21:09:54 +1000

    factory.py file writing sprint; product.py effect of Factory.produce() method; template.py repurposed into coding reference

--- parsers/version2/factory.py
+++ parsers/version2/factory.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
-from template import Template
+
+
 class Factory:
-    def __init__(self, filepath, sections):
-        self.filepath:str = filepath
+    def __init__(self, datasheet, destination, sections):
+        self.datasheet:str = datasheet
+        self.destination:str = destination
         self.sections:tuple[str] = sections
         self.specs:dict[str, list[str]] = {}
-    def specify(self) -> None:
+    def specify(self) -> bool:
         self.specs = {section: [] for section in self.sections}
         try:
-            with open(self.filepath, 'r') as reader:
+            with open(self.datasheet, 'r') as reader:
                 lines = [line.strip() for line in reader.readlines()
                          if "Page" not in line
                          and "MoTeC C125 Dash Manager" not in line]
         except FileNotFoundError:
-            print(f"Factory: Could not find {self.filepath}")
+            print(f"Factory: Could not find {self.datasheet}")
+            return False
         except Exception as e:
             print(f"Factory: Unknown error {e}")
+            return False
         else:
             for index, section in enumerate(self.sections):
                 if index+1 == len(self.sections): continue
@@ -25,13 +29,34 @@ class Factory:
             penultimate:int = lines.index(self.sections[-1])
             final:int = len(lines)
             self.specs[self.sections[-1]] = lines[penultimate:final]
+            return True
+        
+    def produce(self, *functions) -> None:
+        with open(self.destination, 'w') as writer:
+            writer.write("#!/usr/bin/env python3\n")
+            writer.write("class ParsePackage:\n\tpass\n\n")
+            # stuff
+            writer.write("class TestPackage:\n\tpass\n\n")
+            # stuff
+            writer.write("if __name__ == \"__main__\":\n\tTestPackage.runAll()")
 
 if __name__ == "__main__":
     print(".")
-    path = r"parsers\version2\motec.txt"
+    ref = r"parsers\version2\motec.txt"
+    draft = r"parsers\version2\product.py"
     headers = ("Summary Information", "Used Channels", "Channels By Function",
                "RS232", "CAN", "Courier C125", "Calculations", "User Conditions",
                "Alarms", "Incomplete Channels", "Unused Channels")
-    test = Factory(path, headers)
+    test = Factory(ref, draft, headers)
     test.specify()
     [print(item, len(test.specs[item])) for item in test.specs]
+    test.produce("RS232", "CAN")
+
+'''
+Currently the template needs to...
+allow for parsing a category of motec data (function naming bit)
+in terms of what metrics it offers (result dictionary bit)
+and how to parse them out of motec (if-else bit)
+
+it would be good to have a testing class for it but that's assuming behaviours
+'''
---
+++ parsers/version2/product.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+class ParsePackage:
+	pass
+
+class TestPackage:
+	pass
+
+if __name__ == "__main__":
+	TestPackage.runAll()
